4fca5e9e418f17a24170cd8b4eda8c425df21022
Deric Le <dericle77@gmail.com>
2024-10-14 19:26:20 -0500

    radix

--- MPI_Builds/algorithms/radixsort.h
+++ MPI_Builds/algorithms/radixsort.h
@@ -6,12 +6,36 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <cmath>
+#include <algorithm>
 
 #include <caliper/cali.h>
 #include <caliper/cali-manager.h>
 #include <adiak.hpp>
 
+#include <vector>
 
-int radix_sort(std::vector<unsigned int> main_vector, MPI_Comm* worker_comm, char** argv);
+// globals
+constexpr int b = 32;          // number of bits for integer
+constexpr int g = 8;           // group of bits for each scan
+constexpr int N = b / g;       // number of passes
+constexpr int B = 1 << g;      // number of buckets, 2^g
+
+// MPI tags constants, offset by max bucket to avoid collisions
+constexpr int COUNTS_TAG_NUM = B + 1;
+constexpr int PRINT_TAG_NUM = COUNTS_TAG_NUM + 1;
+constexpr int NUM_TAG = PRINT_TAG_NUM + 1;
+
+class Bucket {
+public:
+    std::vector<int> array;
+
+    void add_item(int item) {
+        array.push_back(item);
+    }
+};
+
+unsigned bits(unsigned x, int k, int j);
+std::vector<int> radix_sort(std::vector<int>& a, std::vector<Bucket>& buckets, const int P, const int rank, int& n);
 
 #endif
+
---
+++ MPI_Builds/generate_data.cpp
@@ -0,0 +1,120 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <vector>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+#include "algorithms/decentralized_generation.h"
+
+int main (int argc, char *argv[]) {
+    int pid, num_processors;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&pid);
+    MPI_Comm_size(MPI_COMM_WORLD,&num_processors);
+    unsigned int global_seq_size = 102;
+
+    if (num_processors >= 2) {
+        std::vector<unsigned int> local_seq;
+
+        /* test case 1: sorted */
+        local_seq.clear();
+        local_seq = decentralized_generation(pid, num_processors, global_seq_size, 0);
+        for (unsigned int i = 0; i < num_processors; ++i) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (pid == i) {
+                printf ("DEBUG [p%d]: seq = {", pid);
+                for (unsigned int j = 0; j < local_seq.size(); ++j){
+                    printf("%d ", local_seq.at(j));
+                }
+                printf("}.\n");
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (pid == 0) {
+            printf("\n");
+        }
+
+        /* test case 2: reverse sorted */
+        local_seq.clear();
+        local_seq = decentralized_generation(pid, num_processors, global_seq_size, 1);
+        for (unsigned int i = 0; i < num_processors; ++i) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (pid == i) {
+                printf ("DEBUG [p%d]: seq = {", pid);
+                for (unsigned int j = 0; j < local_seq.size(); ++j){
+                    printf("%d ", local_seq.at(j));
+                }
+                printf("}.\n");
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (pid == 0) {
+            printf("\n");
+        }
+
+        /* test case 3: 1% pertubed */
+        local_seq.clear();
+        local_seq = decentralized_generation(pid, num_processors, global_seq_size, 2);
+        for (unsigned int i = 0; i < num_processors; ++i) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (pid == i) {
+                printf ("DEBUG [p%d]: seq = {", pid);
+                for (unsigned int j = 0; j < local_seq.size(); ++j){
+                    printf("%d ", local_seq.at(j));
+                }
+                printf("}.\n");
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (pid == 0) {
+            printf("\n");
+        }
+
+        /* test case 4: random */
+        local_seq.clear();
+        local_seq = decentralized_generation(pid, num_processors, global_seq_size, 3);
+        for (unsigned int i = 0; i < num_processors; ++i) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (pid == i) {
+                printf ("DEBUG [p%d]: seq = {", pid);
+                for (unsigned int j = 0; j < local_seq.size(); ++j){
+                    printf("%d ", local_seq.at(j));
+                }
+                printf("}.\n");
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (pid == 0) {
+            printf("\n");
+        }
+
+        /* test case 5: more processors than global seq */
+        local_seq.clear();
+        local_seq = decentralized_generation(pid, num_processors, 0, 2);
+        for (unsigned int i = 0; i < num_processors; ++i) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (pid == i) {
+                printf ("DEBUG [p%d]: seq = {", pid);
+                for (unsigned int j = 0; j < local_seq.size(); ++j){
+                    printf("%d ", local_seq.at(j));
+                }
+                printf("}.\n");
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (pid == 0) {
+            printf("\n");
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
