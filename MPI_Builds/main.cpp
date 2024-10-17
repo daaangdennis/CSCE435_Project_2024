1bc69e8502cc6e682bd2d15b089ffbd4ef9d52ed
Jose-Ortiz47 <josepascualos17@tamu.edu>
2024-10-17 15:31:46 -0500

    Organized Cali, Edited ReadME, modified grace file cleaned Main Jose Dennis Sam

--- MPI_Builds/main.cpp
+++ MPI_Builds/main.cpp
@@ -19,33 +19,12 @@
 #include "algorithms/decentralized_generation.h"
 #include "algorithms/sort_validation.h"
 
-// temporarily used for data generation
-#include <algorithm>
-#include <random>
-
-#define MASTER 0 /* taskid of first task */
-#define DEBUG 0 /* print debugging */
-/*
-example debug usage:
-
-#if DEBUG
-printf("will print if debug is true")
-#endif
-*/
-
 int main(int argc, char *argv[])
 {
     CALI_CXX_MARK_FUNCTION;
-    const char* main = "main";                              // main function
-    const char* data_init_runtime = "data_init_runtime";    // generating data during the program
-    const char* comm = "comm";                              // all communication related functions
-    const char* comm_small = "comm_small";                  // send/broadcast few values
-    const char* comm_large = "comm_large";                  // sending all local values
-    const char* comp = "comp";                              // all computation related functions
-    const char* comp_small = "comp_small";                  // sorting a few values
-    const char* comp_large = "comp_large";                  // sorting values in the array
-    const char* correctness_check = "correctness_check";    // checking correctness of array
-
+    const char *data_init_runtime = "data_init_runtime"; // generating data during the program
+    const char *correctness_check = "correctness_check"; // checking correctness of array
+    const char *sort_runtime = "sort_runtime";           // sorting region
 
     if (argc != 4)
     {
@@ -57,15 +36,18 @@ int main(int argc, char *argv[])
     std::string array_type, algorithm;
 
     // try to parse the variables
-    try {
+    try
+    {
         array_size = round((pow(2, std::stoi(argv[1]))));
         array_type = std::string(argv[2]);
         algorithm = std::string(argv[3]);
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
         printf("Invalid args.\n");
         return 1;
     }
-    
+
     int taskid, numtasks;
 
     // init mpi
@@ -78,7 +60,7 @@ int main(int argc, char *argv[])
         MPI_Abort(MPI_COMM_WORLD, 1);
         return 1;
     }
-    
+
     // init caliper, use config in mpi.grace_job
     cali_init();
     cali::ConfigManager mgr;
@@ -86,125 +68,103 @@ int main(int argc, char *argv[])
 
     // Metadata collection
     adiak::init(NULL);
-    adiak::launchdate();    // launch date of the job
-    adiak::libraries();     // Libraries used
-    adiak::cmdline();       // Command line used to launch the job
-    adiak::clustername();   // Name of the cluster
-    adiak::value("algorithm", algorithm); // The name of the algorithm you are using (e.g., "merge", "bitonic")
-    adiak::value("programming_model", "mpi"); // e.g. "mpi"
-    adiak::value("data_type", "unsigned int"); // The datatype of input elements (e.g., double, int, float)
+    adiak::launchdate();                                     // launch date of the job
+    adiak::libraries();                                      // Libraries used
+    adiak::cmdline();                                        // Command line used to launch the job
+    adiak::clustername();                                    // Name of the cluster
+    adiak::value("algorithm", algorithm);                    // The name of the algorithm you are using (e.g., "merge", "bitonic")
+    adiak::value("programming_model", "mpi");                // e.g. "mpi"
+    adiak::value("data_type", "unsigned int");               // The datatype of input elements (e.g., double, int, float)
     adiak::value("size_of_data_type", sizeof(unsigned int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    adiak::value("input_size", array_size); // The number of elements in input dataset (1000)
-    adiak::value("input_type", array_type); // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
-    adiak::value("num_procs", numtasks); // The number of processors (MPI ranks)
-    adiak::value("scalability", "strong"); // The scalability of your algorithm. choices: ("strong", "weak")
-    adiak::value("group_num", 25); // The number of your group (integer, e.g., 1, 10)
-    adiak::value("implementation_source", "online"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
+    adiak::value("input_size", array_size);                  // The number of elements in input dataset (1000)
+    adiak::value("input_type", array_type);                  // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
+    adiak::value("num_procs", numtasks);                     // The number of processors (MPI ranks)
+    adiak::value("scalability", "strong");                   // The scalability of your algorithm. choices: ("strong", "weak")
+    adiak::value("group_num", 25);                           // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "handwritten");    // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
 
-    // start main region
-    // std::vector<unsigned int> main_vector(array_size, 0);
-    CALI_MARK_BEGIN(main);
-
-    // MPI_Comm worker_comm;
-    // MPI_Comm_split(MPI_COMM_WORLD, taskid == MASTER ? MPI_UNDEFINED : 0, taskid, &worker_comm);
-
-    // /********************** MASTER **************************/
-    // if (taskid == MASTER)
-    // {
-    //     #if DEBUG
-    //     printf("arraysize: %u\n arraytype: %s\n algo: %s\n",
-    //         array_size,
-    //         array_type.c_str(),
-    //         algorithm.c_str());
-    //     #endif
-    // }
-    
-    #if DEBUG
     printf("arraysize: %u\n arraytype: %s\n algo: %s\n",
-        array_size,
-        array_type.c_str(),
-        algorithm.c_str());
-    #endif
-    
-    /****************** generate the data (temp) ***********************/
-    // main_vector={1,2,3,4};
-
-    // if (taskid==0){
-    //     for (unsigned int element : main_vector) {
-    //         printf("&d",element);
-    //     }
-    // }
+           array_size,
+           array_type.c_str(),
+           algorithm.c_str());
 
     CALI_MARK_BEGIN(data_init_runtime);
     std::vector<unsigned int> main_vector;
     /********************** generate the data *****************************/
-    if (array_type=="sorted"){
+    if (array_type == "sorted")
+    {
         main_vector = decentralized_generation(taskid, numtasks, array_size, 0);
     }
-    else if (array_type=="reverse"){
+    else if (array_type == "reverse")
+    {
         main_vector = decentralized_generation(taskid, numtasks, array_size, 1);
     }
-    else if (array_type=="perturbed"){
+    else if (array_type == "perturbed")
+    {
         main_vector = decentralized_generation(taskid, numtasks, array_size, 2);
     }
-    else if (array_type=="random"){
+    else if (array_type == "random")
+    {
         main_vector = decentralized_generation(taskid, numtasks, array_size, 3);
     }
-    else{
+    else
+    {
         printf("Invalid array type");
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
     CALI_MARK_END(data_init_runtime);
 
     /********************** sort the data *****************************/
-    if (algorithm=="bitonic"){
+
+    CALI_MARK_BEGIN(sort_runtime);
+    if (algorithm == "bitonic")
+    {
         bitonic_sort(main_vector, taskid, numtasks, MPI_COMM_WORLD);
     }
-    else if (algorithm=="column"){
-        //column_sort(main_vector, array_size, worker_comm);
+    else if (algorithm == "column")
+    {
+        // column_sort(main_vector, array_size, worker_comm);
     }
-    else if (algorithm=="merge"){
+    else if (algorithm == "merge")
+    {
         mergesort(main_vector, taskid, numtasks, MPI_COMM_WORLD);
     }
-    else if (algorithm=="radix"){
-        //radix_sort(main_vector, &worker_comm, argv);
+    else if (algorithm == "radix")
+    {
+        // radix_sort(main_vector, &worker_comm, argv);
     }
-    else if (algorithm=="sample"){
-        //sample_sort(main_vector, array_size, worker_comm);
+    else if (algorithm == "sample")
+    {
+        // sample_sort(main_vector, array_size, worker_comm);
     }
-    else{
+    else
+    {
         printf("Invalid algorithm");
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
+    CALI_MARK_END(sort_runtime);
 
     // validate it
     CALI_MARK_BEGIN(correctness_check);
     bool is_everyone_sorted = sort_validation(main_vector, taskid, numtasks, MPI_COMM_WORLD);
-    // for (unsigned int i = 0; i < numtasks; ++i){
-    //     MPI_Barrier(MPI_COMM_WORLD);
-    //     if (i == taskid){
-    //         printf("DEBUG [p%d]: is_everyone_sorted = %d.\n", taskid, is_everyone_sorted);
-    //     }
-    //     MPI_Barrier(MPI_COMM_WORLD);
-    // }
-    // MPI_Barrier(MPI_COMM_WORLD);
-    if (taskid == 0) {
-        if (is_everyone_sorted) {
+
+    if (taskid == 0)
+    {
+        if (is_everyone_sorted)
+        {
             printf("Sequence is sorted.");
         }
-        else {
+        else
+        {
             printf("Sequence is NOT sorted.");
         }
     }
     CALI_MARK_END(correctness_check);
 
-    // end main region
-    CALI_MARK_END(main);
-
     // flush Caliper output before finalizing MPI
-    mgr.stop(); mgr.flush();
+    mgr.stop();
+    mgr.flush();
     MPI_Finalize();
 
     return 0;
 }
-
--- README.md
+++ README.md
@@ -1,29 +1,72 @@
 # Project_2024
 
 ### setup
+
+```
+./build.sh
+make
+```
+
+### run main
+
+```
+sbatch mpi.grace_job <processor> <array size> <array type> <algorithm>
+
+note: array size is the power of the input size
+
+ex:
+sbatch mpi.grace_job 2 16 random radix
+
+processors = 2 | array size = 2^16 | array type = random | algorithm = radix
+```
+
+### options for array type:
+
+```
+sorted
+random
+perturbed
+reverse
+```
+
+### options for algorithms:
+
+```
+bitonic
+column
+merge
+radix
+sample
+```
+
+<!-- ### setup
+
 ```
 ./build.sh
 make
 ```
 
 ### array type flags
+
 ```
 0: sorted
 1: reverse
-2: 1% perturbed 
+2: 1% perturbed
 3: random
 ```
 
 ### run radix sort
+
 ```
 sbatch radix.grace_job 2^<array size> <array type> <processes>
 sbatch radix.grace_job 10 3 2
 ```
 
 ### run grace tests
+
 ```
 sbatch tests/generation-mpi.grace_job 20 2
 sbatch tests/validation-mpi.grace_job 20 2
 ```
 
-This repository contains the necessary materials for the project, including a template for the report
+This repository contains the necessary materials for the project, including a template for the report -->
